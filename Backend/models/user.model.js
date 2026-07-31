58af157ad803e98763b56a1a856be3c375bb66bb
Yash Nagve Patil <yashnagve5@gmail.com>
2026-07-31 11:51:06 +0530

    user model created

---
+++ Backend/models/user.model.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose"
+import bcrypt from "bcryptjs"
+
+const userSchema = new mongoose.Schema({
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+
+    },
+  contact:{
+    type:String,
+
+  }  ,
+  password:{
+    type:String,
+    required:true,    
+  },
+  role:{
+    type:String,
+    enum:["buyer","seller"],
+    default:"buyer"
+  },
+  fullName:{
+    type:String,
+    required:true
+  },
+})
+
+userSchema.pre("save",async function (){
+    if (!this.isModified("password")) return;
+
+    const hash = await bcrypt.hash(this.password,10);
+    this.password = hash
+
+})
+
+userSchema.methods.comaparePasword = async function(password){
+    return await bcrypt.compare(password,this.password);
+    
+} 
+
+const userModel = mongoose.model("user",userSchema);
+
+ export default  userModel;
