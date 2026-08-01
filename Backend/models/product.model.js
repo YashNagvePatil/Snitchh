a4e4f37b274b7f40c0c73ba190e2b0f57e777fb3
Yash Nagve Patil <yashnagve5@gmail.com>
2026-08-01 14:21:12 +0530

    product model is created

---
+++ Backend/models/product.model.js
@@ -0,0 +1,68 @@
+import mongoose from "mongoose";
+import priceSchema from "./price.schema.js";
+const productSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+   
+    seller:{
+        type:String,
+        ref:"user",
+        required:true
+    },
+    price: {  
+        type: priceSchema,
+        required: true
+      },
+
+      image:[
+        {
+            url:{
+                type:String,
+                required:true   
+            }
+        }
+      ],
+
+      variants:[
+        {
+            images:[{
+                    url:{
+                        type:String,
+                        required:true
+                    }                   
+            }
+        ],
+        stock:{
+            type:Number,
+            default:0
+        },
+        attributes:{
+            type:Map,
+            of:String
+        },
+
+           price:{
+                amount:{
+                    type:Number,
+                    required:true
+           },
+           currency:{
+                type:String,
+                enum:["USD","EUR","GBP","JPY","INR"],
+                default:"INR"   
+            }
+           }
+         }
+      ]
+}, {
+    timestamps: true
+});
+
+export const productModel = mongoose.model("product", productSchema);
+export default productModel 
--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,4 +1,5 @@
 import express from "express"
+import cors from "cors"
 import morgan from "morgan"
 import cookieParser from "cookie-parser"
 import authRouter from "../routes/auth.routes.js"
@@ -12,12 +13,17 @@ import authRouter from "../routes/auth.routes.js"
  app.use(express.urlencoded({extended:true}))
  app.use(cookieParser());
 
+ app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true,
+    methods:["GET","POST","PUT","DELETE"]
+ }))
 
  app.get('/',(req,res) => {
     res.send("Hello World!");
  })
 
- app.use("/api/auth",authRouter)
+ app.use("/auth/api",authRouter)
  
 
  export default app
