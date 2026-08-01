31bbff856e63d860a12fc5ed598bdef10185f0e2
Yash Nagve Patil <yashnagve5@gmail.com>
2026-08-01 14:37:28 +0530

    product validator added

--- Backend/routes/products.route.js
+++ Backend/routes/products.route.js
@@ -1,1 +1,18 @@
-import 
+import express from "express";
+import multer from "multer";
+
+
+ const upload = multer({
+    storage:multer.memoryStorage(),
+    limits:{fileSize:5 * 1024 * 1024}, // 5MB
+ })
+
+  const router = express.Router();
+
+  /**
+   * @route POST /api/products/
+   * @description create a new product
+   * @access private
+   */
+
+  router.post("/",authenticateSeller,upload.array("images",7),createProduct)
